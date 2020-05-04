194c598ecc13ef0741300067448cb916fc5a7374
Mokana <dileep.mokana@philips.com>
2020-05-04 12:09:03 +0530

    Updated

--- Array/Decompress_Run-Length_Encoded_List.cpp
+++ Array/Decompress_Run-Length_Encoded_List.cpp
@@ -6,6 +6,28 @@ public:
         
         for(int i=0;i<len;i+=2)
         {
+            /*vector.insert(position,size,value) */
+            decompressed.insert(decompressed.end(),nums[i],nums[i+1]);
+        }
+        return decompressed;
+    }
+};
+
+
+
+
+
+
+
+
+/*class Solution {
+public:
+    vector<int> decompressRLElist(vector<int>& nums) {
+        int len = nums.size();
+        vector<int>decompressed;
+        
+        for(int i=0;i<len;i+=2)
+        {
             for(int j =0;j<nums[i];j++)
             {
                 decompressed.push_back(nums[i+1]);
@@ -13,4 +35,4 @@ public:
         }
         return decompressed;
     }
-};
+};*/
