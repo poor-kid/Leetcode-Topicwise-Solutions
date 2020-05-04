6a50a4559a9e898ebf31349f804ff070473f35d4
poor-kid <dileepmokana@gmail.com>
2020-05-05 01:44:41 +0530

    Updated

--- String/Defanging_an_ip_address.cpp
+++ String/Defanging_an_ip_address.cpp
@@ -5,6 +5,7 @@ public:
         string str;
         for(int i=0;i<len;i++)
         {
+            /*Appending [.] in place of . */ 
             if(address[i] == '.')
             {
                 str += '[';
