ae9ca215ca9a858a97d8958529f61e5d3fbdf8ea
poor-kid <dileepmokana@gmail.com>
2020-05-05 01:53:39 +0530

    Updated

--- String/Defanging_an_ip_address.cpp
+++ String/Defanging_an_ip_address.cpp
@@ -3,6 +3,7 @@ public:
     string defangIPaddr(string address) {
         int len = address.length();
         string str;
+        /* Appending to String */
         for(int i=0;i<len;i++)
         { 
             if(address[i] == '.')
