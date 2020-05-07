50946e2fe616760fa63e24ea60ac7446a6bd97d7
poor-kid <dileepmokana@gmail.com>
2020-05-07 09:54:37 +0530

    Updated

---
+++ Binary_Search/Kth_Smallest_Element_in_BST.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    
+    void inorder(TreeNode* root,vector<int> &arr)
+    {
+            if(root != NULL)
+            {
+             inorder(root->left,arr);
+             arr.push_back(root->val);
+             inorder(root->right,arr);
+            }
+    }
+public:
+    int kthSmallest(TreeNode* root, int k) {
+        
+        vector<int> res;
+         inorder(root,res);
+        return res[k-1];
+    }
+};
